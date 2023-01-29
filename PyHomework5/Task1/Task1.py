ccb319252fb37c99dfa0e385e02cec0d9c8d34c8
KonstantinVasiliev <grafit21@mail.ru>
2023-01-29 12:59:26 +0500

    save all f for task 1

--- PyHomework5/Task1/Task1.py
+++ PyHomework5/Task1/Task1.py
@@ -1,20 +1,29 @@
 # Создадим новый текстовый файл
 
-txt = open("newtxt.txt", "w", encoding="utf-8")
-txt.write("Ой как долго я маялся абв что бы абв в файлах увидеть вместо абв каракулей абв родненькую кириллицу! абв абв ")
+txt = open("Task1_newtxt.txt", "w", encoding="utf-8")
+txt.write("Ой как долабвго я маялся абв что бы абв в файлах увидеть вместо абв каракулей абв родненькуюабв кириллицу! абв абв ")
 txt.close()
 
 # Чтение из файла
 
-intxt = open("newtxt.txt", 'r', encoding="utf-8")
+intxt = open("Task1_newtxt.txt", 'r', encoding="utf-8")
 text = intxt.read()
-print(f'Исходный текст:     {text}')                                        # Читаем в строку
-new_lst = [j for j in text.split() if j not in 'абв']                        # Изменяем строку в списке с условием
-print(f'Результат операций: {" ".join(new_lst)}')                            # Выводим список через строку
+print(f'Исходный текст:     {text}')                                       
 
+# Преобразуем в список. Выполняем условие в функции
 
+def del_words(str):
+    str = list(filter(lambda x: 'абв' not in x, str.split()))
+    return " ".join(str)
 
+text = del_words(text)
+print(f'Результат операций: {text}')
 
+# Записываем в новый файл
+
+txt2 = open("Task1_cortxt.txt", "w", encoding="utf-8")
+txt2.write(text)
+txt2.close()
 
 
 
